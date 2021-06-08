75ec5c935b8f5137ac6a8283b549187fe94772d7
Craig <craigwilsonmusic@gmail.com>
2021-06-08 15:32:26 +0100

    MVP done

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
+
 public class Bus {
 
-    private Destination destination;
-    private Capacity capacity;
+    private String destination;
+    private int capacity;
+    private ArrayList<Person> passengers;
 
+    public Bus(String destination){
+        this.destination = destination;
+        this.capacity = 40;
+        this.passengers = new ArrayList<>();
+    }
+
+    public String getDestination() {
+        return this.destination;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int getPassengerCount() {
+        return this.passengers.size();
+    }
+
+    public void addPassenger(Person person) {
+        int numberOfPassengers = getPassengerCount();
+        if (numberOfPassengers < this.capacity) {
+            this.passengers.add(person);
+        }
+    }
+
+    public void removePassenger() {
+        this.passengers.remove(0);
+    }
 }
--- src/test/java/TestBus.java
+++ src/test/java/TestBus.java
@@ -8,15 +8,40 @@ import static org.junit.Assert.assertEquals;
 public class TestBus {
 
     private Bus bus;
+    private Person person;
 
     @Before
     public void before(){
         bus = new Bus("Haymarket");
+        person = new Person();
     }
 
     @Test
     public void busHasDestination(){
         assertEquals("Haymarket", bus.getDestination());
+    }
+
+    @Test
+    public void busHasCapacity(){
+        assertEquals(40,bus.getCapacity());
+    }
+
+    @Test
+    public void busStartsWithNoPassengers() {
+        assertEquals(0, bus.getPassengerCount());
+    }
+
+    @Test
+    public void busCanAddPassengers(){
+        bus.addPassenger(person);
+        assertEquals(1, bus.getPassengerCount());
+    }
+
+    @Test
+    public void canRemovePassengerFromBus(){
+        bus.addPassenger(person);
+        bus.removePassenger();
+        assertEquals(0, bus.getPassengerCount());
     }
 
 }
